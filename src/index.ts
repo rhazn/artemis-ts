97b5ceedf8b18360fc9bd5feb9dbf38f92fc27d8
Philip Heltweg <pheltweg@gmail.com>
2019-11-02 17:26:03 +0100

    feat: Added main entry point and typings for package json as well as npm publish scripts

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+export * from "./prolog";
+
+export * from "./annotations/EntityTemplate";
+export * from "./annotations/Mapper";
+export * from "./annotations/Pooled";
+
+export * from "./blackboard/BlackBoard";
+export * from "./blackboard/SimpleTrigger";
+export * from "./blackboard/Trigger";
+export * from "./blackboard/TriggerMultiCondition";
+export * from "./blackboard/TriggerStateType";
+
+export * from "./core/Artemis";
+export * from "./core/Aspect";
+export * from "./core/Component";
+export * from "./core/ComponentManager";
+export * from "./core/ComponentMapper";
+export * from "./core/ComponentPool";
+export * from "./core/ComponentType";
+export * from "./core/ComponentTypeFactory";
+export * from "./core/Entity";
+export * from "./core/EntityManager";
+export * from "./core/EntityObserver";
+export * from "./core/EntitySystem";
+export * from "./core/IEntityTemplate";
+export * from "./core/Manager";
+export * from "./core/PooledComponent";
+export * from "./core/World";
+
+export * from "./managers/GroupManager";
+export * from "./managers/PlayerManager";
+export * from "./managers/TagManager";
+export * from "./managers/TeamManager";
+
+export * from "./systems/DelayedEntityProcessingSystem";
+export * from "./systems/EntityProcessingSystem";
+export * from "./systems/IntervalEntityProcessingSystem";
+export * from "./systems/IntervalEntitySystem";
+export * from "./systems/VoidEntitySystem";
+
+export * from "./utils/Bag";
+export * from "./utils/BitSet";
+export * from "./utils/HashMap";
+export * from "./utils/ImmutableBag";
+export * from "./utils/Map";
+export * from "./utils/MathUtils";
+export * from "./utils/Signal";
+export * from "./utils/Timer";
+export * from "./utils/TrigLUT";
+export * from "./utils/UUID";
